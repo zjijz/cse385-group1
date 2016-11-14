4b3e006e84e4ba3fa57db5c8e94495c86d320a5a
GNPMobile <patakygn@miamioh.edu>
2016-11-14 15:16:27 -0500

    Fixed some issues with serving app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PagesComponent } from './pages/pages.component';
-import { HomComponent } from './pages/home/hom/hom.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { LoanScrollerComponent } from './components/loan-scroller/loan-scroller.component';
 import { HoldScrollerComponent } from './components/hold-scroller/hold-scroller.component';
 import { FeedWidgetComponent } from './components/feed-widget/feed-widget.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PagesComponent,
-    HomComponent,
     HomeComponent,
     FeedComponent,
     LoanScrollerComponent,
